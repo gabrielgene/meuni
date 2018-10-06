1403377f7708616110662136a544703901592b08
Gabriel Genê <gabrielgene97@gmail.com>
2018-10-06 10:34:39 -0300

    Add reply

--- src/components/reply/index.jsx
+++ src/components/reply/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
+import { withStyles } from '@material-ui/core/styles';
 import ReplyPage from '../../pages/reply';
 
-const Reply = ({ open }) => (
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+const Reply = ({ open, classes }) => (
   <Modal open={open} >
-    <ReplyPage />
+    <div className={classes.root}>
+      <ReplyPage />
+    </div>
   </Modal>
 );
 
-export default Reply;
+export default withStyles(styles, { withTheme: true })(Reply);
